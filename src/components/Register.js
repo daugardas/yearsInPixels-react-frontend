13341efa5491bfc3f45124793e72a2ae3d0afa78
daugardas <daugismokykla@gmail.com>
2018-07-09 01:17:59 +0300

    rewrite Register component using new components

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,10 @@ import Recaptcha from 'react-recaptcha';
 import injectSheet from 'react-jss';
 
 import Loader from './Loader';
+import TextInput from './TextInput';
+import PasswordInput from './PasswordInput';
+import EmailInput from './EmailInput';
+import SubmitButton from './SubmitButton';
 
 const styles = {
   container: {
@@ -27,80 +31,6 @@ const styles = {
       color: `#364d6b`
     }
   },
-  iconContainer: {
-    display: `flex`,
-    float: `right`,
-    width: `40px`,
-    height: `45.5px`,
-    background: `#f3fbff`,
-    alignItems: `center`,
-    justifyContent: `center`,
-    border: `1px solid #d8efff`,
-    borderRadius: `25px`,
-    borderTopLeftRadius: `0`,
-    borderBottomLeftRadius: `0`,
-    '& i': {
-      cursor: `pointer`,
-      margin: `0 10px`,
-      color: `#83a2c7`,
-      transition: `color 0.2s ease`,
-    },
-    '& i:hover': {
-      color: `#00060e`
-    }
-  },
-  inputPass: {
-    fontFamily: `Indie Flower, cursive`,
-    float: `right`,
-    marginLeft: `20px`,
-    background: `#f3fbff`,
-    borderRadius: `25px`,
-    border: ` 1px solid #d8efff`,
-    padding: ` 5px 10px 5px 15px`,
-    lineHeight: `35px`,
-    borderTopRightRadius: `0`,
-    borderBottomRightRadius: `0`,
-    fontSize: `16px`,
-    width: `249px`,
-    caretColor: `#a1d2ff`,
-    transition: `box-shadow 0.5s ease`,
-    '&:focus': {
-      boxShadow: `0px 0px 0px 2px #a9dbff`
-    }
-  },
-  button: {
-    padding: `10px`,
-    width: `130px`,
-    backgroundColor: `#eef9ff`,
-    fontSize: `25px`,
-    margin: `0 10px`,
-    border: `5px solid #dbf0ff`,
-    borderRadius: `30px`,
-    transition: `font-weight 0.3s ease, transform 0.3s ease, color 0.3s ease`,
-    alignSelf: `center`,
-    '&:hover': {
-      fontWeight: `700`,
-      cursor: `pointer`,
-      transform: `scale(1.02)`
-    }
-  },
-  input: {
-    fontFamily: `'Indie Flower', cursive`,
-    float: `right`,
-    fontSize: `22px`,
-    marginLeft: `20px`,
-    background: `#f3fbff`,
-    width: `290px`,
-    borderRadius: `25px`,
-    border: `1px solid #d8efff`,
-    padding: `5px 10px 5px 15px`,
-    lineHeight: `35px`,
-    caretColor: `#a1d2ff`,
-    transition: `box-shadow 0.5s ease`,
-    '&:focus': {
-      boxShadow: `0px 0px 0px 2px #a9dbff`
-    }
-  },
   recaptcha: {
     alignSelf: 'center'
   }
@@ -114,44 +44,43 @@ class Register extends Component {
     this.state = {
       loading: false,
       captchaVerified: false,
-      captchaToken: null
+      captchaToken: null,
+      email: '',
+      username: '',
+      password: '',
+      confPassword: ''
     };
   }
-  render() {
+  render(){
     const { classes } = this.props;
+    const { username, email, password, confPassword } = this.state;
     const form = this.state.loading ? (
       <Loader />
     ) : (
         <form method="post">
           <div className={classes.inputContainer}>
             <label htmlFor="username">Username:</label>
-            <input required id="username" className={classes.input} name="username" type="text" />
+            <TextInput onChange={this.handleUsernameChange.bind(this)} value={username} />
           </div>
           <div className={classes.inputContainer}>
             <label htmlFor="email">Email:</label>
-            <input required id="email" className={classes.input} name="email" type="email" />
+            <EmailInput onChange={this.handleEmailChange.bind(this)} value={email} />
           </div>
           <div className={classes.inputContainer}>
             <label htmlFor="password">Password:</label>
-            <div className={classes.iconContainer}>
-              <i id="pass-icon" className="fas fa-eye-slash hide" onClick={this.showPass}></i>
-            </div>
-            <input required className={classes.inputPass} type="password" name="password" id="password" />
+            <PasswordInput value={password} onChange={this.handlePassChange.bind(this)} />
           </div>
           <div className={classes.inputContainer}>
             <label htmlFor="conf-password">Confirm password:</label>
-            <div className={classes.iconContainer}>
-              <i id="conf-icon" className="fas fa-eye-slash hide" onClick={this.showConfPass}></i>
-            </div>
-            <input required className={classes.inputPass} type="password" name="conf-password" id="conf-password" />
+            <PasswordInput value={confPassword} onChange={this.handleConfPassChange.bind(this)} />
           </div>
           <Recaptcha
             className={classes.recaptcha}
             sitekey="6Lf1HFwUAAAAAAEIUqAnwrGrXJtqqL_ya6tPV7bS"
             verifyCallback={this.recaptchaVerify}
           />
-          <button type="submit" onClick={this.register} className={classes.button}>Register</button>
-        </form >
+          <SubmitButton onClick={this.register}>Register</SubmitButton>
+        </form>
       );
     return (
       <div className={classes.container}>
@@ -159,66 +88,72 @@ class Register extends Component {
       </div>
     );
   }
+  handleUsernameChange(val){
+    this.setState({ username: val });
+  }
+  handleEmailChange(val) {
+    this.setState({ email: val });
+  }
+  handlePassChange(val) {
+    this.setState({ password: val });
+  }
+  handleConfPassChange(val) {
+    this.setState({ confPassword: val });
+  }
   recaptchaVerify(response) {
     this.setState({ captchaVerified: true, captchaToken: response });
   }
-  register(e) {
+  register() {
     const { createNotification } = this.props;
-    e.preventDefault();
-    if (this.state.captchaVerified) {
+    const { email, username, password, confPassword, captchaToken, captchaVerified } = this.state;
+    if (captchaVerified) {
       this.setState({ loading: true });
-      this.username = document.getElementById('username').value;
-      this.email = document.getElementById('email').value;
-      this.password = document.getElementById('password').value;
-      this.confPassword = document.getElementById('conf-password').value;
       let makeRequest = true;
+
       // validate email and passwords
-      if (this.password !== this.confPassword) {
+      if (password !== confPassword) {
         makeRequest = false;
-        createNotification('error', "Passwords do not match.")
-        document.getElementById('password').setCustomValidity("Passwords do not match.")
-        document.getElementById('conf-password').setCustomValidity("Passwords do not match.")
+        createNotification('error', "Passwords do not match.");
       }
-      if (this.password.length < 8) {
+
+      if (password.length < 8) {
         makeRequest = false;
-        createNotification('error', "Minimum password length is 8 characters.")
-        document.getElementById('password').setCustomValidity("Minimum password length is 8 characters.")
-      } else if (this.password.length > 128) {
+        createNotification('error', "Minimum password length is 8 characters.");
+      } else if (password.length > 128) {
         makeRequest = false;
-        createNotification('error', "Maximum password length is 128 characters.")
-        document.getElementById('password').setCustomValidity("Maximum password length is 128 characters.")
+        createNotification('error', "Maximum password length is 128 characters.");
       } else {
         let passwordRegExp = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))/g;
-        if (!passwordRegExp.test(this.password)) {
+        if (!passwordRegExp.test(password)) {
           makeRequest = false;
-          createNotification('error', "Password must be least one lowercase letter and one number or one lowecase letter and uppercase letter.")
-          document.getElementById('password').setCustomValidity("Password must be least one lowercase letter and one number or one lowecase letter and uppercase letter.")
+          createNotification('error', "Password must be least one lowercase letter and one number or one lowecase letter and uppercase letter.");
         }
       }
+
       // check email
       let emailRegEx = /^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if (!emailRegEx.test(this.email)) {
+      if (!emailRegEx.test(email)) {
         makeRequest = false;
-        createNotification('error', "Email adress is invalid.")
-        document.getElementById('email').setCustomValidity("Email adress is invalid.");
+        createNotification('error', "Email adress is invalid.");
       }
+
       // check username
-      if (this.username.length < 5) {
-        createNotification('error', "Username must be higher than 5 characters.")
-        document.getElementById('username').setCustomValidity("Username must be higher than 5 characters.");
+      if (username.length < 5) {
+        createNotification('error', "Username must be higher than 5 characters.");
         makeRequest = false;
-      } else if (this.username.length > 35) {
+      } else if (username.length > 35) {
         makeRequest = false;
-        createNotification('error', "Username must be lower than 32 characters.")
-        document.getElementById('username').setCustomValidity("Username must be lower than 32 characters.");
+        createNotification('error', "Username must be lower than 32 characters.");
       }
+
       if (makeRequest) {
         let data = {
-          'username': this.username,
-          'email': this.email,
-          'password': this.password,
-          'recaptchaResponse': this.state.captchaToken
+          'username': username,
+          'email': email,
+          'password': password,
+          'recaptchaResponse': captchaToken
         }
+
         let httpRequest = new XMLHttpRequest();
         httpRequest.open('POST', `https://api.yearsinpixels.com/api/register`, true);
         httpRequest.setRequestHeader("Content-Type", "application/json");
@@ -233,6 +168,7 @@ class Register extends Component {
                 createNotification('error', response.error);
               } else {
                 let response = JSON.parse(httpRequest.responseText);
+
                 if (response.hasOwnProperty('errors')) {
                   response.errors.map(err => console.log(err));
                 } else if (response.hasOwnProperty('error')) {
@@ -241,6 +177,7 @@ class Register extends Component {
                   console.log(response.message);
                 }
               }
+
               this.setState({ loading: false });
             }
           } catch (e) {
@@ -254,42 +191,6 @@ class Register extends Component {
     } else {
       createNotification('error', "Verify that you're not a robot.");
       this.setState({ loading: false });
-    }
-  }
-  showPass() {
-    let iconEl = document.getElementById('pass-icon');
-    if (iconEl.classList.contains('fa-eye-slash')) {
-      iconEl.classList.remove('fa-eye-slash');
-      iconEl.classList.add('fa-eye');
-    } else {
-      iconEl.classList.remove('fa-eye');
-      iconEl.classList.add('fa-eye-slash');
-    }
-    let passInputEl = document.getElementById('password');
-    if (passInputEl.type === 'password') {
-      passInputEl.type = 'text';
-      passInputEl.style.fontSize = "22px";
-    } else {
-      passInputEl.type = 'password';
-      passInputEl.style.fontSize = "16px";
-    }
-  }
-  showConfPass() {
-    let iconEl = document.getElementById('conf-icon');
-    if (iconEl.classList.contains('fa-eye-slash')) {
-      iconEl.classList.remove('fa-eye-slash');
-      iconEl.classList.add('fa-eye');
-    } else {
-      iconEl.classList.remove('fa-eye');
-      iconEl.classList.add('fa-eye-slash');
-    }
-    let passInputEl = document.getElementById('conf-password');
-    if (passInputEl.type === 'password') {
-      passInputEl.type = 'text';
-      passInputEl.style.fontSize = "22px";
-    } else {
-      passInputEl.type = 'password';
-      passInputEl.style.fontSize = "16px";
     }
   }
   componentDidMount() {
