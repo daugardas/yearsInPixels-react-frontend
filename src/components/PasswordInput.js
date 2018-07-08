e0303d74699d230798113d66c2daa4e693383920
daugardas <daugismokykla@gmail.com>
2018-07-09 00:36:07 +0300

    Edit PasswordInput component

--- src/components/PasswordInput.js
+++ src/components/PasswordInput.js
@@ -7,21 +7,21 @@ const styles = {
     width: 317.2
   },
   inputPass: {
-    fontFamily: `Indie Flower, cursive`,
-    float: `right`,
-    marginLeft: `20px`,
-    background: `#f3fbff`,
-    borderRadius: `25px`,
-    border: ` 1px solid #d8efff`,
-    padding: ` 5px 10px 5px 15px`,
-    lineHeight: `35px`,
-    borderTopRightRadius: `0`,
-    borderBottomRightRadius: `0`,
-    width: `249px`,
-    caretColor: `#a1d2ff`,
-    transition: `box-shadow 0.5s ease`,
+    fontFamily: 'Indie Flower, cursive',
+    float: 'right',
+    marginLeft: 20,
+    background: '#f3fbff',
+    borderRadius: 25,
+    border: '1px solid #d8efff',
+    padding: '5px 10px 5px 15px',
+    lineHeight: '35px',
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    width: 249,
+    caretColor: '#a1d2ff',
+    transition: 'box-shadow 0.5s ease',
     '&:focus': {
-      boxShadow: `0px 0px 0px 2px #a9dbff`
+      boxShadow: '0px 0px 0px 2px #a9dbff'
     }
   },
   hiddenFontSize: {
@@ -31,31 +31,31 @@ const styles = {
     fontSize: 22
   },
   iconContainer: {
-    display: `flex`,
-    float: `right`,
-    width: `40px`,
-    height: `45.5px`,
-    background: `#f3fbff`,
-    alignItems: `center`,
-    justifyContent: `center`,
-    border: `1px solid #d8efff`,
-    borderRadius: `25px`,
-    borderTopLeftRadius: `0`,
-    borderBottomLeftRadius: `0`,
+    display: 'flex',
+    float: 'right',
+    width: 40,
+    height: 45.5,
+    background: '#f3fbff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid #d8efff',
+    borderRadius: 25,
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
     '& i': {
-      cursor: `pointer`,
-      margin: `0 10px`,
-      color: `#83a2c7`,
-      transition: `color 0.2s ease`,
+      cursor: 'pointer',
+      margin: '0 10px',
+      color: '#83a2c7',
+      transition: 'color 0.2s ease',
     },
     '& i:hover': {
-      color: `#00060e`
+      color: '#00060e'
     }
   }
 };
 
 class PasswordInput extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       hidden: true
@@ -67,18 +67,18 @@ class PasswordInput extends Component {
     return (
       <div className={classes.container}>
         <div className={classes.iconContainer}>
-          { hidden ? <i className="fas fa-eye-slash hide" onClick={this.handleVisibility.bind(this)}></i>: <i className="fas fa-eye hide" onClick={this.handleVisibility.bind(this)}></i>} 
+          <i className={`fas ${hidden ? 'fa-eye-slash' : 'fa-eye'} hide`} onClick={this.handleVisibility.bind(this)}></i>
         </div>
-        <input required className={`${classes.inputPass} ${hidden ? classes.hiddenFontSize:classes.shownFontSize}`} type={ hidden ? 'password':'text'} value={value} onChange={this.handleChange.bind(this)} />
+        <input required className={`${classes.inputPass} ${hidden ? classes.hiddenFontSize : classes.shownFontSize}`} type={hidden ? 'password' : 'text'} value={value} onChange={this.handleChange.bind(this)} />
       </div>
     );
   }
-  handleChange(e){
+  handleChange(e) {
     const { onChange } = this.props;
     onChange(e.target.value);
   }
-  handleVisibility(){
-    this.setState({ hidden: !this.state.hidden});
+  handleVisibility() {
+    this.setState({ hidden: !this.state.hidden });
   }
 }
 
